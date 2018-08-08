781f7ad55796ed96268883ec957ca2e98539438d
rodrigo-tinini <tinini.89@gmail.com>
2018-08-08 19:57:40 -0300

    Implementing new methods for better run the static cases

---
+++ graphs/static_experiments.py
@@ -0,0 +1,17 @@
+import importlib
+import simpy
+import functools
+import random
+import time
+from enum import Enum
+import numpy as np
+from scipy.stats import norm
+import scipy as sp
+import scipy.stats
+import matplotlib.pyplot as plt
+import copy
+import graph as g
+
+#general variables
+rrhs_amount = 5
+#fog nodes amount
