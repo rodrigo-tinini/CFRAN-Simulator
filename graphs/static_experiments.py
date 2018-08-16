8e93ef3324bf4a65552480abd7784bfd0c3e0df2
rodrigo-tinini <tinini.89@gmail.com>
2018-08-16 01:08:44 -0300

    Experiments ready

--- graphs/static_experiments.py
+++ graphs/static_experiments.py
@@ -12,21 +12,120 @@ import matplotlib.pyplot as plt
 import copy
 import graph as g
 
+def genLogs():
+	#iterate over each scheduling policy
+	for i in sched_pol:
+		#power consumption
+		with open('/home/tinini/Área de Trabalho/iccSim/CFRAN-Simulator/graphs/static/logs/power_consumption_{}.txt'.format(i),'a') as filehandle:  
+		    filehandle.write("{}\n\n".format(i))
+		    filehandle.writelines("%s\n" % p for p in power_consumption["{}".format(i)])
+		    filehandle.write("\n")
+		    #filehandle.write("\n")
+		with open('/home/tinini/Área de Trabalho/iccSim/CFRAN-Simulator/graphs/static/logs/execution_time_{}.txt'.format(i),'a') as filehandle:  
+		    filehandle.write("{}\n\n".format(i))
+		    filehandle.writelines("%s\n" % p for p in execution_time["{}".format(i)])
+		    filehandle.write("\n")
+		    #filehandle.write("\n")
+
+#fog nodes amount
+fog_amount = 5
+#scheduling policies
+sched_pol = []
+sched_pol.append("all_random")
+sched_pol.append("cloud_first_all_fogs")
+sched_pol.append("cloud_first_random_fogs")
+#sched_pol.append("fog_first")
+sched_pol.append("most_loaded")
+sched_pol.append("least_loaded")
+#sched_pol.append("least_cost")
+#sched_pol.append("least_cost_active_ratio")
+#sched_pol.append("most_loaded_bandwidth")
+#sched_pol.append("least_loaded_bandwidth")
+#sched_pol.append("all_random")
+#sched_pol.append("big_ratio")
+#sched_pol.append("small_ratio")
 #log variables
-power_consumption = []
-execution_time = []
+power_consumption = {}
+execution_time = {}
+for i in sched_pol:
+	power_consumption["{}".format(i)] = []
+	execution_time["{}".format(i)] = []
+
+def setExperiment(gp, rrhs, fogs):
+	divided = int(rrhs/fogs)
+	g.addRRHs(gp, 0, divided, "0")
+	g.addRRHs(gp, divided, divided*2, "1")
+	g.addRRHs(gp, divided*2, divided*3, "2")
+	g.addRRHs(gp, divided*3, divided*4, "3")
+	g.addRRHs(gp, divided*4, divided*5, "4")
 
 #Assign VPON Heuristic simulations
 #add 5 rrhs per execution
-#5 rrhs
 g.rrhs_amount = 5
-#fog nodes amount
+rs = g.rrhs_amount
+#main loop
+for s in sched_pol:
+	g.rrhs_amount = 5
+	rs = g.rrhs_amount
+	print("Executing {}".format(s))
+	for i in range(32):
+		#print(g.rrhs_amount)
+		importlib.reload(g)
+		g.rrhs_amount = rs
+		gp = g.createGraph()
+		g.createRRHs()
+		g.addFogNodes(gp, fog_amount)
+		setExperiment(gp, g.rrhs_amount, fog_amount)
+		for i in range(len(g.rrhs)):
+			g.startNode(gp, "RRH{}".format(i))
+			g.actives_rrhs.append("RRH{}".format(i))
+		if s == "all_random":
+			g.allRandomVPON(gp)
+		#elif s == "fog_first":
+		#	g.fogFirst(gp)
+		elif s == "cloud_first_all_fogs":
+			g.assignVPON(gp)
+		elif s == "cloud_first_random_fogs":
+			g.randomFogVPON(gp)
+		elif s == "most_loaded":
+			g.assignMostLoadedVPON(gp)
+		elif s == "least_loaded":
+			g.assignLeastLoadedVPON(gp)
+		#elif s == "least_cost":
+		#	g.leastCostNodeVPON(gp)
+		#elif s == "least_cost_active_ratio":
+		#	g.leastCostLoadedVPON(gp)
+		#most and least loaded that considers the bandwidth available on each node midhaul
+		#elif s == "most_loaded_bandwidth":
+		#	g.assignMostLoadedVPONBand(gp)
+		#elif s == "least_loaded_bandwidth":
+		#	g.assignLeastLoadedVPONBand(gp)
+		#elif s == "big_ratio":
+		#	g.assignBigRatioVPON(gp)
+		#elif s == "small_ratio":
+		#	g.assignSmallRatioVPON(gp)
+		start_time = g.time.clock()
+		mincostFlow = g.nx.max_flow_min_cost(gp, "s", "d")
+		execution_time[s].append(g.time.clock() - start_time)
+		power_consumption[s].append(g.overallPowerConsumption(gp))
+		g.rrhs_amount += 5
+		rs = g.rrhs_amount
+print(power_consumption)
+genLogs()
+
+
+'''
 gp = g.createGraph()
 g.createRRHs()
-print(len(g.rrhs))
+#print(len(g.rrhs))
 #for i in rrhs:
 #  print(i.id)
-g.addFogNodes(gp, 5)
+g.addFogNodes(gp, fog_amount)
+setExperiment(gp, g.rrhs_amount, fog_amount)
+for i in range(len(g.rrhs)):
+	g.startNode(gp, "RRH{}".format(i))
+	g.actives_rrhs.append("RRH{}".format(i))
+
 g.addRRHs(gp, 0, 1, "0")
 g.addRRHs(gp, 1, 2, "1")
 g.addRRHs(gp, 2, 3, "2")
@@ -209,3 +308,4 @@ importlib.reload(g)
 
 print(power_consumption)
 print(execution_time)
+'''
